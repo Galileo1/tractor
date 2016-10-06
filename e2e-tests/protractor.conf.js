38175f0e4573588f251cc464c6e74c51ce16bf1c
Varun Gaur <gaur.var@gmail.com>
2016-10-07 09:38:12 +1300

    updating instances

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -10,9 +10,9 @@ exports.config = {
      //    'args': ['--load-extension=' + 'T:\\Test\\Private\\NextGen\\gighmmpiobklfepjocnamgkkbiglidom\\3.2.0_0']
      // }
       'shardTestFiles': true,
-      'maxInstances': 0
+      'maxInstances': 1
 },
-    framework: 'cucumber',
+    framework: 'cucumber',    
     cucumberOpts: {
         require: [
             'support/**/*.js',
