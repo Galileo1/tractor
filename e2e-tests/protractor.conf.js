6b8d9a368877e8e963e1378d3d13af5350c0d066
Varun Gaur <gaur.var@gmail.com>
2016-10-11 11:53:32 +1300

    adding hook file and config file changes

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -2,6 +2,7 @@
 exports.config = {
     allScriptsTimeout: 11000,
     specs: ['features/**/*.feature'],
+    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     // capabilities: { 'browserName': 'chrome' },
     capabilities: {
       'browserName': 'chrome',
@@ -10,7 +11,7 @@ exports.config = {
      //    'args': ['--load-extension=' + 'T:\\Test\\Private\\NextGen\\gighmmpiobklfepjocnamgkkbiglidom\\3.2.0_0']
      // }
       'shardTestFiles': true,
-      'maxInstances': 1
+      'maxInstances': 2
 },
     framework: 'cucumber',    
     cucumberOpts: {
--- e2e-tests/support/hooks.js
+++ e2e-tests/support/hooks.js
@@ -2,8 +2,7 @@
 
 /* eslint-disable no-var, prefer-arrow-callback */
 var cucumber = require('cucumber');
-var reporter = require('cucumber-html-reporter');
-var log = require('npmlog');
+var cucumberHtmlReport = require('cucumber-html-report');
 var path = require('path');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
@@ -11,10 +10,7 @@ var fs = Promise.promisifyAll(require('fs'));
 
 function createReporter () {
     /* eslint-disable new-cap */
-    var jsonFormatter = cucumber.Listener.JsonFormatter();    
-    var mkDir =  getFileName('tractorDir_', null) 
-    var baseDirReporting = path.join(__dirname, '../', 'report', mkDir)
-    fs.mkdirAsync(baseDirReporting)   
+    var jsonFormatter = cucumber.Listener.JsonFormatter();
     var outputDir = path.join(__dirname, '../', 'report')
     jsonFormatter.log = jsonReportWriter;
     return jsonFormatter;
@@ -25,36 +21,28 @@ function createReporter () {
         fs.writeFileAsync(cucumberReport, content)
          /* eslint-disable prefer-arrow-callback */
         .then(function () {
-            return htmlReportWriter(cucumberReport);
+           // return htmlReportWriter(cucumberReport);
+           console.log("written to file");
         })
          /* eslint-disable prefer-template */
         .catch(function (error) {
-            log.error('Failed to save test results to json file. ' + error);
+            console.log('Failed to save test results to json file. ' + error);
         });
     }
 
-    function htmlReportWriter (cucumberReport) {        
+    function htmlReportWriter (cucumberReport) {
         var htmlFileName = getFileName('tractorReport_', 'html');
-        var outputFile = path.join(outputDir , htmlFileName);
-        var options = {
-            theme: 'bootstrap',
-            jsonDir: outputDir,
-            output: outputFile,
-            reportSuiteAsScenarios: true            
-        };
-        reporter.generate(options);
+        var report = new cucumberHtmlReport({
+            source: cucumberReport,
+            dest: outputDir,
+            name: htmlFileName
+        });
+        report.createReport();
     }
 
     function getFileName (file, extension) {
         /* eslint-disable prefer-template */
-        if (extension !== null) { 
-
-            return file + new Date().toLocaleString().replace(/[\/\\:]/g,"-") + '.' + extension;
-
-        } else  {
-
-            return file + new Date().toLocaleString().replace(/[\/\\:]/g,"-");
-        }
+        return file + new Date().toLocaleString().replace(/[\/\\:]/g,"-") + '.' + extension;
     }
 }
 
