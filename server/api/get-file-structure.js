c941411bdebb763dc721d050417b3722b4997bb2
Varun Gaur <gaur.var@gmail.com>
2016-10-21 12:53:14 +1300

    putting back changes for get file structure

--- server/api/get-file-structure.js
+++ server/api/get-file-structure.js
@@ -8,7 +8,6 @@ export default { handler };
 function handler (request, response) {
     let { type } = request.params;
 
-    fileStructure.read();
     let structure = fileStructure.getStructure(type);
     response.send(structure);
 }
