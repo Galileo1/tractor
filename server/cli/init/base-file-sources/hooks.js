032df72ae7a72149b7feb3f5f5dba6cb7c7ccc37
Varun Gaur <gaur.var@gmail.com>
2016-12-21 10:06:26 +1300

    updating hook file

--- server/cli/init/base-file-sources/hooks.js
+++ server/cli/init/base-file-sources/hooks.js
@@ -6,11 +6,10 @@ var path = require('path');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 
-
 function createReporter () {
     /* eslint-disable new-cap */
     var jsonFormatter = cucumber.Listener.JsonFormatter();
-    var outputDir = path.join(__dirname, '../', 'report')
+    var outputDir = path.join(__dirname, '../', 'report', 'now')
     jsonFormatter.log = jsonReportWriter;
     return jsonFormatter;
 
@@ -28,8 +27,9 @@ function createReporter () {
         /* eslint-disable prefer-template */
         return file + new Date().toLocaleString().replace(/[\/\\:]/g, "-") + '.' + extension;
     }
-}
 
+}
+        
 module.exports = function () {
     var reporter = createReporter();
     this.registerListener(reporter);
